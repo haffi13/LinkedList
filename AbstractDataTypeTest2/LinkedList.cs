6d4da5a1fbdbc2413554021cd4298edbd7896dc8
Hafsteinn Fannar <haffi13@gmail.com>
2018-02-03 14:07:42 +0100

    Add project files.

---
+++ AbstractDataTypeTest2/LinkedList.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractDataTypeTest2
+{
+    class LinkedList
+    {
+        private class ListItem
+        {
+            public object Item { get; }
+            public ListItem Next { get; set; }
+
+            public ListItem(object o)
+            {
+                Item = o;
+                Next = null;
+            }
+
+            public override string ToString()
+            {
+                return Item.ToString();
+            }
+        }
+
+        private ListItem firstItem = null;
+
+        private ListItem lastItem = null;
+
+        private int itemCount = 0;
+
+        public int Count
+        {
+            get { return itemCount; }
+        } 
+
+        public object First { get { return firstItem; } }//First and last return null if the list is empty
+        public object Last { get { return lastItem; } }
+        public bool ListIsEmpty
+        {
+            get { return itemCount == 0; }
+        }
+
+
+        public void InsertFirst(object o)
+        {
+            ListItem itemToAdd = new ListItem(o);
+            if(firstItem == null)
+            {
+                firstItem = new ListItem(itemToAdd);
+                lastItem = firstItem; 
+            }
+            else
+            {
+                
+                    for (int i = 0; i < Count; i++)
+                    {
+                        itemToAdd.Next = firstItem;
+                    }
+                
+                firstItem = itemToAdd;
+            }
+            itemCount++;
+        }
+
+        public void InsertLast(object o)
+        {
+            ListItem itemToAdd = new ListItem(o);
+            if(lastItem == null)
+            {
+                InsertFirst(itemToAdd);//last item is instanciated in InsertFirst is there is no previous item in the list
+            }
+            else
+            {
+                ListItem currentLast = lastItem;
+                currentLast.Next = itemToAdd;
+                lastItem = itemToAdd;
+                itemCount++;
+            }
+        }
+        
+        public object Items(int index)
+        {
+            ListItem temp = firstItem;
+            object ret = temp;
+            if (index < 0)
+            {
+                //exception - might return a message but that's probably not correct
+                return null;
+            }
+            else
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    temp = temp.Next;
+                    ret = temp;
+                }
+                return ret;
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            ListItem temp = firstItem;
+
+            if (index < 0 || index >= Count)
+            {
+                if (ListIsEmpty)
+                {
+                    firstItem = null;
+                }
+                Console.WriteLine("Removal not possible");
+            }
+            else if(index == 0)
+            {
+                firstItem = temp.Next;
+                itemCount--;
+            }
+            else
+            {
+                for (int i = 0; i < index-1; i++) //index-1 cuz 0 based, might have counter start at -1 instead
+                {
+                    temp = temp.Next;//gets the list item before the item to remove
+                }
+                temp.Next = temp.Next.Next;//sets the next list item after the list item before the item to remove as the item after the item to remove
+                itemCount--;
+            } 
+        }
+
+        public override string ToString()
+        {
+            string ret = string.Empty;
+            ListItem listItem = firstItem;
+            for (int i = 0; i < Count; i++)
+            {
+                    ret += "|" + listItem.ToString();
+                    listItem = listItem.Next;
+            }
+            return ret;
+        }
+    }
+}
---
+++ AbstractDataTypeTest2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractDataTypeTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+
+            Names names = new Names();
+            Names names1 = new Names();
+            Names names2 = new Names();
+            Names names3 = new Names();
+            Names names4 = new Names();
+            Names names5 = new Names();
+            Names names6 = new Names();
+            Names names7 = new Names();
+            Names names8 = new Names();
+            Names names9 = new Names();
+
+
+            names.Name = "Lars";
+            names1.Name = "Peter";
+            names2.Name = "Gunnlöður";
+            names3.Name = "Hafsteinn";
+            names4.Name = "Lebron";
+            names5.Name = "Trevor";
+            names6.Name = "Kristín";
+            names7.Name = "Ananas";
+            names8.Name = "Last";
+            names9.Name = "First";
+
+            //remove is zero based, if you want to remove 3rd item on the list you RemoveAt(2);
+
+            Console.WriteLine(linkedList.First);
+            Console.ReadKey();
+
+            linkedList.InsertLast(names.Name);
+            linkedList.InsertLast(names1.Name);
+            linkedList.InsertLast(names2.Name);
+
+            linkedList.InsertFirst(names3.Name);
+            linkedList.InsertFirst(names4.Name);
+
+            linkedList.InsertLast(names5.Name);
+            linkedList.InsertLast(names6.Name);
+
+            linkedList.InsertFirst(names7.Name);
+
+            //linkedList.RemoveAt(0);
+
+            linkedList.InsertLast(names8.Name);
+
+            linkedList.InsertFirst(names9.Name);
+
+            //linkedList.InsertFirst(" ");
+            //linkedList.InsertFirst(6);//can add different data types into the list...
+            //linkedList.RemoveAt(2);
+            //Console.WriteLine(linkedList.Items(0));
+            //Console.WriteLine(linkedList.Items(4));
+            //Console.WriteLine(linkedList.Items(-1));//returns null, doesnt cause havoc
+
+            Console.WriteLine(linkedList.ToString());
+
+            //Console.WriteLine(linkedList.Count);
+
+            /*for (int i = linkedList.Count; i >= 0; i--) //shows you can remove from the first position until the list is empty
+            {                                           //also shows that you don't get exception if you try to remove from an empty list
+                linkedList.RemoveAt(0);
+                Console.WriteLine(linkedList.ToString());
+            }*/
+
+            //linkedList.RemoveAt(linkedList.Count);//shows you don't get an exception if you try to remove a item at index where there is no item 
+
+
+            Console.ReadKey();
+        }
+    }
+}
